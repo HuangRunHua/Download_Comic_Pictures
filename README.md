a6b941bc8d8856b68cd6a605354f01dbcf0b12f3
Huang Runhua <45262463+HuangRunHua@users.noreply.github.com>
2022-01-27 20:55:07 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Download_Comic_Pictures
+ 本程序实现金田一37岁事件薄的图片的下载
---
+++ picture_fetch.py
@@ -0,0 +1,36 @@
+import requests
+import numpy as np
+from sympy import *
+from bs4 import BeautifulSoup
+np.seterr(divide='ignore', invalid='ignore')
+
+"""
+本程序用于下载金田一37岁事件薄的图片
+链接: https://www.maofly.com/manga/10736.html
+
+在使用本程序时需要在Chrome或Safari上预先加载好图片再将网页内容下载保存为html文件
+html文件内容应当如下:
+<div class="img-content" style="display:flex;flex-direction: column;">
+	<img class="img-fluid show-pic" src="https://mao.mhtupian.com/uploads/img/26847/348716/01.jpg">
+	<img src="https://mao.mhtupian.com/uploads/img/26847/348716/02-03.jpg" class="img-fluid">
+    ......
+</div>
+"""
+
+def get_all_pics(path):
+    # 打开并读取php文件
+    phpFile = open(path, 'r', encoding='utf-8')
+    phpHandle = phpFile.read()
+    soup = BeautifulSoup(phpHandle, 'lxml')
+    # 用于保存图片的命名
+    pic_index = 1
+
+    for img in soup.select('img'):
+        pic_name = str(pic_index) + ".jpg"
+        f = open(pic_name,'wb')
+        f.write(requests.get(img['src']).content)
+        pic_index = pic_index + 1
+        f.close()
+
+
+get_all_pics('01.html')
